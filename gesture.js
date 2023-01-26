ae7325c6a9a28448ad7210a3351d8eab0b9f633d
redmattre <pierpaolo.ovarini@gmail.com>
2023-01-26 15:29:20 +0100

    added gestures

--- gesture.js
+++ gesture.js
@@ -5,17 +5,22 @@ const gestureArea = document.getElementById("gesture-area");
 const manager = new Hammer.Manager(gestureArea);
 
 //gesture recognizers
-manager.add(new Hammer.Swipe({ direction: Hammer.DIRECTION_HORIZONTAL }));
-manager.add(new Hammer.Tap({ event: 'doubletap', taps: 2 }));
+manager.add(new Hammer.Swipe({ direction: Hammer.DIRECTION_ALL, threshold: 100 }));
+//manager.add(new Hammer.Swipe({ direction: Hammer.DIRECTION_VERTICAL, threshold: 150 }));
+manager.add(new Hammer.Tap({ event: 'doubletap', taps: 2, threshold: 20, posThreshold: 100 }));
 
 // Get the swipe and double tap counter elements
 const swipeLefthCounterElement = document.getElementById("swipe-counter-left");
 const swipeRightCounterElement = document.getElementById("swipe-counter-right");
+const swipeUpCounterElement = document.getElementById("swipe-counter-up");
+const swipeDownCounterElement = document.getElementById("swipe-counter-down");
 const doubleTapCounterElement = document.getElementById("double-tap-counter");
 
 //init counters
 let swipeRightCounter = 0;
 let swipeLeftCounter = 0;
+let swipeUpCounter = 0;
+let swipeDownCounter = 0;
 let doubleTapCounter = 0;
 
 //events
@@ -29,6 +34,18 @@ manager.on("swiperight", function(event)
 {
   swipeRightCounter++;
   swipeRightCounterElement.innerHTML = `right swipes: ${swipeRightCounter}`;
+});
+
+manager.on("swipeup", function(event) 
+{
+  swipeUpCounter++;
+  swipeUpCounterElement.innerHTML = `up swipes: ${swipeUpCounter}`;
+});
+
+manager.on("swipedown", function(event) 
+{
+  swipeDownCounter++;
+  swipeDownCounterElement.innerHTML = `down swipes: ${swipeDownCounter}`;
 });
 
 manager.on("doubletap", function(event) {
